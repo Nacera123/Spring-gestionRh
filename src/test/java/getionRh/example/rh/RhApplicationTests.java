60e81a73f9c5effc4d6a8aa332f21207d68667bc
nacera123 <nace.slimane@gmail.com>
2024-01-20 19:09:06 +0100

    creation du projet

---
+++ src/test/java/getionRh/example/rh/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package getionRh.example.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
